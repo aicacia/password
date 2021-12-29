9e4b8f3ecbb0e8207e59ae821f7767e1821edb29
nathanfaucett <nathanfaucett@users.noreply.github.com>
2021-12-29 15:50:24 +0000

    deploy: b7dd3f077acba752118f655358aee36bf9816cea

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const timestamp = 1640654909648;
+const timestamp = 1640793020862;
 const build = [
-  "/password/_app/start-832efdfa.js",
+  "/password/_app/start-614e7796.js",
   "/password/_app/assets/start-d5b4de3e.css",
-  "/password/_app/pages/__layout.svelte-3de27fcd.js",
-  "/password/_app/assets/pages/__layout.svelte-8cebbbf9.css",
-  "/password/_app/error.svelte-028e89a1.js",
-  "/password/_app/pages/index.svelte-f7581bb9.js",
-  "/password/_app/chunks/vendor-94822eda.js",
+  "/password/_app/pages/__layout.svelte-76bf7ee3.js",
+  "/password/_app/assets/pages/__layout.svelte-c0ad2e42.css",
+  "/password/_app/error.svelte-1510c2c6.js",
+  "/password/_app/pages/index.svelte-d0207867.js",
+  "/password/_app/pages/privacy-policy.svelte-46641eea.js",
+  "/password/_app/chunks/vendor-a66a53b1.js",
   "/password/_app/assets/vendor-86f8c920.css",
   "/password/_app/chunks/paths-28a87002.js",
   "/password/_app/chunks/preload-helper-04e99934.js",
+  "/password/_app/chunks/Layout-9f43be66.js",
   "/password/_app/chunks/widget-3e963cc8.js"
 ];
 const files = [
