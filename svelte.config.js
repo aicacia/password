35c7c17b082082416da4e0c471f5126423e869c1
Nathan Faucett <nathanfaucett@gmail.com>
2022-06-09 21:06:43 -0400

    fix build even more

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,14 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			trailingSlash: 'always',
+			precompress: true
+		}),
+		appDir: 'internal',
 		paths: process.env.USE_BASE_PATH
 			? {
 					base: '/password'
