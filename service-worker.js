1e6b02cb215144bcb0fa55ce1a21ccf3460b0446
nathanfaucett <nathanfaucett@users.noreply.github.com>
2022-06-15 10:56:08 +0000

    deploy: 4dd34c8dd6f74f7930e7740f93f153facb2cee03

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 const build = [
-  "/secrets/internal/immutable/start-cbdc9c90.js",
-  "/secrets/internal/immutable/pages/__layout.svelte-7c485db1.js",
+  "/secrets/internal/immutable/start-39287309.js",
+  "/secrets/internal/immutable/pages/__layout.svelte-51b5392b.js",
   "/secrets/internal/immutable/assets/pages/__layout.svelte-0bc20e30.css",
-  "/secrets/internal/immutable/error.svelte-6fc140ef.js",
-  "/secrets/internal/immutable/pages/index.svelte-1d77d4bc.js",
-  "/secrets/internal/immutable/pages/privacy-policy.svelte-8ed01a30.js",
-  "/secrets/internal/immutable/pages/terms-of-service.svelte-330e20ce.js",
-  "/secrets/internal/immutable/chunks/index-09b57bf3.js",
-  "/secrets/internal/immutable/chunks/index-fa36ae4b.js",
+  "/secrets/internal/immutable/error.svelte-67b8f836.js",
+  "/secrets/internal/immutable/pages/index.svelte-c023f341.js",
+  "/secrets/internal/immutable/pages/privacy-policy.svelte-0a39e765.js",
+  "/secrets/internal/immutable/pages/terms-of-service.svelte-850c1615.js",
+  "/secrets/internal/immutable/chunks/index-b32f51c6.js",
+  "/secrets/internal/immutable/chunks/index-ffc9c32f.js",
   "/secrets/internal/immutable/chunks/paths-396f020f.js",
   "/secrets/internal/immutable/chunks/preload-helper-91b3b7b7.js",
-  "/secrets/internal/immutable/chunks/Layout-fda693ea.js",
+  "/secrets/internal/immutable/chunks/Layout-81174ec1.js",
   "/secrets/internal/immutable/assets/Layout-14f30235.css",
   "/secrets/internal/immutable/chunks/widget-bc58238e.js"
 ];
@@ -20,7 +20,7 @@ const files = [
   "/secrets/manifest.json",
   "/secrets/robots.txt"
 ];
-const version = "1655258466140";
+const version = "1655290556074";
 const worker = self;
 const FILES = `cache${version}`;
 const toCache = build.concat(files);
