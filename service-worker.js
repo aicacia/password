d369ee923b9a5f3ce9fe155a881ecbed5bfe0157
nathanfaucett <nathanfaucett@users.noreply.github.com>
2022-06-16 02:34:27 +0000

    deploy: 680d672d11aa7c6abc87819d98209c4e79c2f2fb

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const build = [
-  "/secrets/internal/immutable/start-751f2b40.js",
+  "/secrets/internal/immutable/start-0d0dec0b.js",
   "/secrets/internal/immutable/pages/__layout.svelte-ef16ba18.js",
   "/secrets/internal/immutable/assets/pages/__layout.svelte-0bc20e30.css",
   "/secrets/internal/immutable/error.svelte-004b9777.js",
-  "/secrets/internal/immutable/pages/index.svelte-8c11fb5a.js",
+  "/secrets/internal/immutable/pages/index.svelte-068ab057.js",
   "/secrets/internal/immutable/pages/privacy-policy.svelte-e5f39db2.js",
   "/secrets/internal/immutable/pages/terms-of-service.svelte-b2e62b7b.js",
   "/secrets/internal/immutable/chunks/index-919745c3.js",
@@ -20,7 +20,7 @@ const files = [
   "/secrets/manifest.json",
   "/secrets/robots.txt"
 ];
-const version = "1655346381956";
+const version = "1655346850275";
 const worker = self;
 const FILES = `cache${version}`;
 const toCache = build.concat(files);
